cc143187851e30cb6bb99b106ba9855950260d00
Dilasha Shakya <dilashakya@gmail.com>
2026-02-09 11:35:01 -0600

    added animation + keyboard press detection

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,10 +1,22 @@
-
+//detecting button press
 for (var i = 0 ; i< document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll("button")[i].addEventListener("click", function(){
         
         var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
+        
+    });
+}
 
-        switch(buttonInnerHTML){
+//detecting key press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+function makeSound(key){
+    switch(key){
             case "w":
                 var tom1 = new Audio ('sounds/tom-1.mp3');
                 tom1.play();
@@ -43,8 +55,13 @@ for (var i = 0 ; i< document.querySelectorAll(".drum").length; i++){
             default: console.log(buttonInnerHTML);
 
         }
-    });
+
 }
 
-// var audio = new Audio ('sounds/tom-1.mp3');
-//         audio.play() 
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." + key)
+    activeButton.classList.add("pressed")
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
